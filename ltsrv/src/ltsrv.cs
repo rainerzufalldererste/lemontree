9b38151f2f1a4ab297bf516e90c8e8405e8cfd7e
Christoph Stiller <c.stiller@live.de>
2022-03-24 10:28:24 +0100

    Preparing for Page Generation

--- ltsrv/src/ltsrv.cs
+++ ltsrv/src/ltsrv.cs
@@ -7,13 +7,51 @@ using LamestWebserver.Serialization;
 public class ltsrv
 {
   public static Analysis _Analysis;
+  public static Info _Info;
 
   [STAThread]
   public static void Main(string[] args)
   {
     _Analysis = Serializer.ReadJsonData<Analysis>(args[0]);
 
-    Console.WriteLine("Deserialized.");
+    Console.WriteLine("Deserialized Analysis.");
+
+    for (int i = 1; i < args.Length;)
+    {
+      switch (args[i])
+      {
+        case "--info":
+          if (i + 1 >= args.Length)
+          {
+            Console.WriteLine($"Argument '{args[i]}' is missing a filepath.");
+            throw new Exception();
+          }
+
+          _Info = Serializer.ReadJsonData<Info>(args[i + 1]);
+
+          if (_Info.productName != _Analysis.productName)
+          {
+            Console.WriteLine($"Product Name Mismatch '{_Info.productName}' != '{_Analysis.productName}'.");
+            throw new Exception();
+          }
+
+          i += 2;
+
+          break;
+
+        default:
+          Console.WriteLine($"Unexpected Argument '{args[i]}'.");
+          throw new Exception();
+      }
+    }
+
+    foreach (var subSystem in _Analysis.states)
+    {
+      foreach (var state in subSystem.value)
+      {
+
+      }
+    }
   }
 }
 
@@ -37,6 +75,14 @@ public struct uint64_t
 
     return new uint64_t(value);
   }
+
+  public static explicit operator uint64_t(ulong v) => new uint64_t(v);
+  public static explicit operator uint64_t(int v) => new uint64_t((ulong)v);
+}
+
+public interface IPage
+{
+  HContainer GetPageContents();
 }
 
 public class Analysis
@@ -83,4 +129,11 @@ public class Operation : TransitionData
   public List<Ref<uint64_t, TransitionData>> nextOperation;
   public List<Ref<uint64_t, TransitionData>> lastOperation;
   public List<Ref<uint64_t, uint64_t>> operationIndexCount;
+}
+
+public class Info
+{
+  public string productName;
+  public List<Ref<uint64_t, List<string>>> states;
+  public List<Ref<uint64_t, List<string>>> operations;
 }
