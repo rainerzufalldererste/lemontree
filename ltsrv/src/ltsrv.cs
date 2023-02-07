5576b0da216f08c89153ead4e3c9779628223612
Christoph Stiller <c.stiller@live.de>
2023-02-07 22:43:59 +0000

    Adding some more Null-Checking and Formatting Tweaks

--- ltsrv/src/ltsrv.cs
+++ ltsrv/src/ltsrv.cs
@@ -513,39 +513,6 @@ public class Home : ElementResponse
   }
 }
 
-public static class ExtentionMethods
-{
-  public static T1 FindItem<T, T1>(this List<Ref<T, T1>> list, T index) where T : IEquatable<T>
-  {
-    foreach (var x in list)
-      if (x.index.Equals(index))
-        return x.value;
-
-    throw new KeyNotFoundException();
-  }
-
-  public static string TryFindItem<T>(this List<Ref<T, string>> list, T index) where T : IEquatable<T>
-  {
-    foreach (var x in list)
-      if (x.index.Equals(index))
-        return x.value;
-
-    return null;
-  }
-
-  public static double ToDouble(this object d)
-  {
-    if (d is double)
-      return (double)d;
-    else if (d is uint64_t)
-      return (double)((uint64_t)d).value;
-    else if (d is int64_t)
-      return (double)((int64_t)d).value;
-    else
-      return Convert.ToDouble(d);
-  }
-}
-
 public struct uint64_t : IEquatable<uint64_t>, IComparable<uint64_t>
 {
   internal ulong value;
